5a1afb4fca6fef8b28e139377a35ed570fa11d92
Ikkyy <msndoikky@hotmail.com>
2019-11-04 13:44:42 -0300

    Class Componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./App.css"
 
-import profile from './assets/pug.jpg'
+import TechList from './components/TechList'
+
+// import profile from './assets/pug.jpg'
 
 function App() {
-    return <img width="150" src={profile}></img>
+    return <TechList />
 }
 
 export default App
---
+++ src/components/TechList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class TechList extends Component {
+
+    state = {
+        techs: [
+            'NodeJS',
+            'ReactJS',
+            'ReactNative'
+        ]
+    };
+
+    render() {
+        console.log(this.state)
+        
+        return (
+            <ul>
+                <li>NodeJS</li>
+                <li>ReactJS</li>
+                <li>ReactNative</li>
+            </ul>
+        )
+    }
+}
+
+export default TechList
