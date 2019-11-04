1063e28142074ae23a25a6111153009621318a3e
Ikkyy <msndoikky@hotmail.com>
2019-11-04 14:42:54 -0300

    Ciclo de Vida do Componente

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -5,12 +5,25 @@ class TechList extends Component {
 
     state = {
         newTech: '',
-        techs: [
-            'NodeJS',
-            'ReactJS',
-            'ReactNative'
-        ]
+        techs: []
     };
+
+    // Executado assim que o componente aparece em tela
+    componentDidMount() {
+        const techs = localStorage.getItem('techs')
+
+        if (techs) {
+            this.setState({ techs: JSON.parse(techs) })
+        }
+    }
+
+    // Executado sempre que houver alterações nas props ou estado
+    componentDidUpdate(_, prevState) {
+        if (prevState.techs !== this.state.techs) {
+            localStorage.setItem('techs', JSON.stringify(this.state.techs))
+        }
+    }
+
 
     handleInputChange = e => {
         this.setState({ newTech: e.target.value })
@@ -36,15 +49,15 @@ class TechList extends Component {
                 <h3>{this.state.newTech}</h3>
                 <ul>
                     {this.state.techs.map(tech =>
-                        <TechItem 
-                        key={tech} 
-                        tech={tech} 
-                        onDelete={() => this.handleDelete(tech)} 
+                        <TechItem
+                            key={tech}
+                            tech={tech}
+                            onDelete={() => this.handleDelete(tech)}
                         />
                     )}
                 </ul>
 
-                <input type="text" onChange={this.handleInputChange} value={this.state.newTech}  />
+                <input type="text" onChange={this.handleInputChange} value={this.state.newTech} />
                 <button type="submit">Enviar</button>
             </form>
         )
